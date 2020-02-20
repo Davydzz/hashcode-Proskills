d1253e237bad155839707180c70de66f7d264a11
Swissnetizen <samu.agarwal@gmail.com>
2020-02-20 18:26:42 +0000

    Implements file parser

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Sibelius Crashed!")
+def parseFile(path):
+	file = open(path, "r")
+
+	lines = file.readlines()
+	# Get number of each from line 0
+	line = lines[0].split()
+	data = {
+		"numberOfBooks": int(line[0]),
+		"numberOfLibraries": int(line[1]),
+		"numberOfDays": int(line[2])
+	}
+	# Extract book scores
+	data["bookScores"] = [int(number) for number in lines[1].split()]
+	libraries = []
+	for libraryNumber in range(0, data["numberOfLibraries"]):
+		index = libraryNumber + 2
+		infoLine = lines[index].split()
+		bookLine = lines[index].split()
+		library = {
+			"numberOfBooks": int(infoLine[0]),
+			"signup": int(infoLine[1]),
+			"rate": int(infoLine[2]),
+			"books": [int(book) for book in bookLine]
+		}
+		libraries.append(library)
+	data["libraries"] = libraries
+	return data
